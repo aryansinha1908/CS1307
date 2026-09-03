2518c4401ea5935df8d03c90c6276ffd56cb88ae
KabootarGod <aryansinha1908@gmail.com>
2026-09-03 16:24:28 +0530

    Assignment 3 completed

--- assignment3.sql
+++ assignment3.sql
@@ -6,7 +6,7 @@ SELECT ID FROM student NATURAL LEFT OUTER JOIN takes WHERE course_id IS NULL;
 
 -- Question 2
 
-SELECT * FROM student NATURAL LEFT OUTER JOIN advisor WHERE i_ID IS NOT NULL;
+SELECT * FROM student NATURAL LEFT OUTER JOIN advisor WHERE i_ID IS NULL;
 
 -- Question 3
 
@@ -36,12 +36,20 @@ DELETE FROM takes WHERE ID = '12345';
 SELECT * FROM takes;
 
 -- 3 E 
+
+DELETE FROM section WHERE course_id = 'CS-001';
 DELETE FROM course WHERE course_id = 'CS-001';
 
 SELECT * FROM takes;
 SELECT * FROM section;
 
 -- 3 F
+DELETE FROM takes WHERE course_id IN
+(
+    SELECT course_id 
+    FROM course 
+    WHERE LOWER(title) LIKE '%advanced%'
+);
 
 -- Question 4
 
@@ -49,9 +57,73 @@ SELECT d1.dept_name FROM department AS d1, department AS d2 WHERE d1.dept_name !
 
 -- Question 5
 
-SELECT course_id, ID FROM takes GROUP BY ID, course_id HAVING count(*) >= 2 ORDER BY course_id;
+SELECT course_id, ID FROM takes GROUP BY ID, course_id HAVING count(*) >= 3 ORDER BY course_id;
 
 
 -- Question 7
 
+SELECT ID, name FROM student WHERE dept_name = 'History' AND LOWER(name) LIKE 'd%'
+AND ID NOT IN (
+    SELECT takes.ID
+    FROM takes
+    JOIN course ON takes.course_id = course.course_id
+    WHERE course.dept_name = 'Music'
+    GROUP BY takes.ID
+    HAVING COUNT(takes.course_id) >= 5
+);
 
+-- Question 8
+
+WITH enrollment AS (
+    SELECT course_id, sec_id, year, semester, COUNT(*) AS num
+    FROM takes
+    GROUP BY course_id, sec_id, year, semester
+)
+SELECT course_id, sec_id, year, semester, num
+FROM enrollment
+WHERE num = (
+    SELECT MAX(num) FROM enrollment
+);
+
+-- Question 9
+
+SELECT i.ID, i.name
+FROM instructor AS i
+WHERE NOT EXISTS (
+    SELECT *
+    FROM teaches AS t
+    JOIN takes AS tk
+      ON t.course_id = tk.course_id
+     AND t.sec_id = tk.sec_id
+     AND t.semester = tk.semester
+     AND t.year = tk.year
+    WHERE t.ID = i.ID
+      AND tk.grade = 'A'
+);
+
+-- Question 9
+
+SELECT i.ID, i.name
+FROM instructor AS i
+WHERE EXISTS (
+    SELECT *
+    FROM teaches AS t
+    JOIN takes AS tk
+      ON t.course_id = tk.course_id
+     AND t.sec_id = tk.sec_id
+     AND t.semester = tk.semester
+     AND t.year = tk.year
+    WHERE t.ID = i.ID
+      AND tk.grade IS NOT NULL
+)
+AND NOT EXISTS (
+    SELECT *
+    FROM teaches AS t
+    JOIN takes AS tk
+      ON t.course_id = tk.course_id
+     AND t.sec_id = tk.sec_id
+     AND t.semester = tk.semester
+     AND t.year = tk.year
+    WHERE t.ID = i.ID
+      AND tk.grade = 'A'
+);
